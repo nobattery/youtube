9a953a1ac3ceafe2780d07a47942796e731ac8db
nobattery <lkg0705@naver.com>
2021-05-22 20:52:49 +0900

    add aixos

--- src/service/youtube.js
+++ src/service/youtube.js
@@ -1,34 +1,36 @@
 //네트워크 부분 App -> Youtube Class(new) - mostPopular, search API 제공
 //에러 처리를 삭제한 이유 : 서비스 API를 소비하는 쪽(컴포넌트 쪽)에서 에러 처리하는 편이 좋기 때문.
+// fetch -> axios로 변환
 class Youtube {
   //this.key : .env 파일에 정의
-  constructor(key) {
-    this.key = key;
-    this.getRequestOptions = {
-      method: "GET",
-      redirect: "follow",
-    };
+  constructor(httpClient) {
+    this.youtube = httpClient;
   }
 
   //Youtube Most Popular API Request
   async mostPopular() {
-    //Most Popular Api- GET - fetch Code from PostMan
-    const response = await fetch(
-      `https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=${this.key}`,
-      this.getRequestOptions
-    );
-    const result_1 = await response.json();
-    return result_1.items;
+    //Json 변환 생략, 가독성 증가, 이전 브라우저 지원 가능
+    const response = await this.youtube.get("videos", {
+      params: {
+        part: "snippet",
+        chart: "mostPopular",
+        maxResults: 25,
+      },
+    });
+    return response.data.items;
   }
 
   //Youtube Search API Request
   async search(query) {
-    const response = await fetch(
-      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=${query}&type=video&key=${this.key}`,
-      this.getRequestOptions
-    );
-    const result_1 = await response.json();
-    return result_1.items.map((item) => ({ ...item, id: item.id.videoId }));
+    const response = await this.youtube.get("search", {
+      params: {
+        part: "snippet",
+        maxResults: 25,
+        type: "video",
+        q: query,
+      },
+    });
+    return response.data.items;
   }
 }
 
